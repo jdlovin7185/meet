e6e9b16186b06d6b86fd1f69822ba2705a54a4a2
Josh Lovin <76003574+jdlovin7185@users.noreply.github.com>
2021-04-02 07:20:32 -0400

    Trying to fix a bug

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -40,19 +40,11 @@ module.exports.getAccessToken = async (event) => {
 };
 
 const { google } = require("googleapis");
-const { toolresults } = require("googleapis/build/src/apis/toolresults");
 const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar("v3");
-/**
- * SCOPES allows you to set access levels; this is set to readonly for now because you don't have access rights to
- * update the calendar yourself. For more info, check out the SCOPES documentation at this link: https://developers.google.com/identity/protocols/oauth2/scopes
- */
+
 const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 
-/**
- * Credentials are those values required to get access to your calendar. If you see “process.env” this means
- * the value is in the “config.json” file. This is a best practice as it keeps your API secrets hidden. Please remember to add “config.json” to your “.gitignore” file.
- */
 const credentials = {
   client_id: process.env.CLIENT_ID,
   project_id: process.env.PROJECT_ID,
@@ -71,21 +63,7 @@ const oAuth2Client = new google.auth.OAuth2(
   redirect_uris[0]
 );
 
-/**
- *
- * The first step in the OAuth process is to generate a URL so users can log in with
- * Google and be authorized to see your calendar. After logging in, they’ll receive a code
- * as a URL parameter.
- *
- */
 module.exports.getAuthURL = async () => {
-  /**
-   *
-   * Scopes array passed to the `scope` option. Any scopes passed must be enabled in the
-   * "OAuth consent screen" settings in your project on your Google Console. Also, any passed
-   *  scopes are the ones users will see when the consent screen is displayed to them.
-   *
-   */
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
     scope: SCOPES,
@@ -94,7 +72,10 @@ module.exports.getAuthURL = async () => {
   return {
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Origin": "*"
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+      "Access-Control-Allow-Methods": "OPTIONS, POST, GET"
     },
     body: JSON.stringify({
       authUrl: authUrl,
