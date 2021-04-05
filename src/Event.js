c038468f03d8b9ef8cb487e29079a829b49010e3
Josh Lovin <76003574+jdlovin7185@users.noreply.github.com>
2021-04-05 08:08:11 -0400

    Event tests

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,40 @@
 import React, { Component } from "react";
 
+
 class Event extends Component {
+
+  state = {
+    hideDetails: false,
+  };
+
+  handleShowDetails = () => {
+    if (this.state.hideDetails === true) {
+      this.setState({ hideDetails: false });
+    } else {
+      this.setState({ hideDetails: true });
+    }
+  };
+
   render() {
-    return <div></div>;
+    const { event } = this.props;
+    return (
+    <div className="Event">
+      <h1>{event.summary}</h1>
+      <p>{event.start.dateTime}</p>
+      <p className="locations">{event.location}</p>
+
+      {this.state.hideDetails && (
+        <div className="detailsCollapsed">
+          <h1>About Event:</h1>
+          <a className="googleEvent" href={event.htmlLink}>
+            See details on Google Calendar
+          </a>
+          <p>{event.description}</p>
+        </div>
+      )}
+      <button className="detailsBtn" onClick={() => this.handleShowDetails()}>{!this.state.handleShowDetails ? "Hide Details" : "Show Details"}</button>
+    </div>
+    )
   }
 }
 
---
+++ src/_tests_/Event.test.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Event from '../Event';
+
+const event = 
+{
+  "kind": "calendar#event",
+  "etag": "\"3181161784712000\"",
+  "id": "4eahs9ghkhrvkld72hogu9ph3e_20200519T140000Z",
+  "status": "confirmed",
+  "htmlLink": "https://www.google.com/calendar/event?eid=NGVhaHM5Z2hraHJ2a2xkNzJob2d1OXBoM2VfMjAyMDA1MTlUMTQwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
+  "created": "2020-05-19T19:17:46.000Z",
+  "updated": "2020-05-27T12:01:32.356Z",
+  "summary": "Learn JavaScript",
+  "description": "Have you wondered how you can ask Google to show you the list of the top ten must-see places in London? And how Google presents you the list? How can you submit the details of an application? Well, JavaScript is doing these. :) \n\nJavascript offers interactivity to a dull, static website. Come, learn JavaScript with us and make those beautiful websites.",
+  "location": "London, UK",
+  "creator": {
+   "email": "fullstackwebdev@careerfoundry.com",
+   "self": true
+  },
+  "organizer": {
+   "email": "fullstackwebdev@careerfoundry.com",
+   "self": true
+  },
+  "start": {
+   "dateTime": "2020-05-19T16:00:00+02:00",
+   "timeZone": "Europe/Berlin"
+  },
+  "end": {
+   "dateTime": "2020-05-19T17:00:00+02:00",
+   "timeZone": "Europe/Berlin"
+  },
+  "recurringEventId": "4eahs9ghkhrvkld72hogu9ph3e",
+  "originalStartTime": {
+   "dateTime": "2020-05-19T16:00:00+02:00",
+   "timeZone": "Europe/Berlin"
+  },
+  "iCalUID": "4eahs9ghkhrvkld72hogu9ph3e@google.com",
+  "sequence": 0,
+  "reminders": {
+   "useDefault": true
+  },
+  "eventType": "default"
+ };
+
+describe('<Event /> component', () => {
+  test('show details button', () => {
+    const EventWrapper = shallow(<Event event={event} />);
+    EventWrapper.setState({ handleShowDetails: true });
+    EventWrapper.find(".detailsBtn").simulate("click");
+  });
+
+  test('hide details button', () => {
+    const EventWrapper = shallow(<Event event={event} />);
+    EventWrapper.setState({ handleShowDetails: false });
+    EventWrapper.find(".detailsBtn").simulate("click");
+  });
+
+  test('renders the correct details', () => {
+    const EventWrapper = shallow(<Event event={event} />);
+    expect(EventWrapper.find(".Event")).toHaveLength(1);
+   });
+
+});
