4bd3dbc7d2aafc0cf80a1755f6537263c1f4dc72
Josh Lovin <76003574+jdlovin7185@users.noreply.github.com>
2021-04-12 10:23:15 -0400

    creates alert for text box city search

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 
 class CitySearch extends Component {
@@ -11,20 +12,36 @@ class CitySearch extends Component {
 
     handleInputChanged = (event) => {
       const value = event.target.value;
+      this.setState({showSuggestions:true});
       const suggestions = this.props.locations.filter((location) => {
         return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
       });
+      if (suggestions.length === 0) {
+        this.setState({
+          query: value,
+          infoText: 'We can not find the city you are looking for. Please try another city',
+        });
+      } else {
+        return this.setState({
+          query: value,
+          suggestions,
+          infoText: ''
+        });
+      }
       this.setState({ 
         query: value, 
-        suggestions
+        suggestions,
+        infoText:''
       });
     }
 
     handleItemClicked = (suggestion) => {
       this.setState({
         query: suggestion,
-        showSuggestions: false
-      });
+        suggestions: [],
+        showSuggestions: false,
+        infoText: ''
+      }); 
 
       this.props.updateEvents(suggestion);
     }
@@ -32,6 +49,7 @@ class CitySearch extends Component {
   render() { 
     return (
       <div className="CitySearch">
+        <InfoAlert text={this.state.infoText} />
         <input
         type="text"
         className="city"
